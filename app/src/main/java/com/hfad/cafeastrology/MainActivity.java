7aae34233c8c574251834bb7d26aa56ae2e683ba
Caprice Williams <capricious@caprices-mbp.quinnipiac.edu>
2022-04-20 17:47:17 -0400

    transitions

--- app/src/main/java/com/hfad/cafeastrology/MainActivity.java
+++ app/src/main/java/com/hfad/cafeastrology/MainActivity.java
@@ -1,15 +1,22 @@
 package com.hfad.cafeastrology;
 
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
